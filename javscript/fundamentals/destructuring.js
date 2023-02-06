2a5b13851d52b5c31640f76f291cab6174807544
MikiWagnr <miguelmontilla96@gmail.com>
2023-02-06 13:19:47 -0500

    init commit

---
+++ javscript/fundamentals/destructuring.js
@@ -0,0 +1,50 @@
+const cars = ['Tesla', ' Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//tesla
+// mercedes
+console.log(randomCar)
+console.log(otherRandomCar)
+
+const employee = {
+    name: 'elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee
+//error because name was not defined anymore
+console.log(name)
+console.log(otherName)
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: ' 6 feet'
+}
+const password = '12345'
+const { password: hashedPassword } = person
+//no hashed password was given therefore it remains undefined
+console.log(password)
+console.log(hashedPassword)
+
+const numbers = [8,2,3,5,6,1,67,12,2]
+const [ ,first ] = numbers
+const [ ,,,second ] = numbers
+const [ ,,,,,,,,third ] = numbers
+//false
+//true
+console.log(first == second)
+console.log(first == third)
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1,5,1,8,3,3]
+}
+const { key } = lastTest
+const { secondkey } = lastTest
+const [ ,willThisWork ] = secondKey
+//secondKey undefined?
+console.log(key)
+console.log(secondKey)
+console.log(secondKey[0])
+console.log(willThisWork)
