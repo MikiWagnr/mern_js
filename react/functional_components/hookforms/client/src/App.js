e537f4889cbc3b4a200d645cf716443825c74aef
MikiWagnr <miguelmontilla96@gmail.com>
2023-02-12 17:14:26 -0500

    hookform

---
+++ react/functional_components/hookforms/client/src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import Form from "./components/forms";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/functional_components/hookforms/client/src/components/forms.jsx
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+
+const Form = () => {
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] =useState('')
+
+    return(
+        <div>
+            <h1>More Form</h1>
+            <form>
+                <div>
+                    <label>First Name</label>
+                    <input type="text" onChange={(e) =>{setFirstName(e.target.value)}} />
+                </div>
+                <div>
+                    <label>Last Name</label>
+                    <input type="text" onChange = {(e) => {setLastName(e.target.value)}} />
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input type="text" onChange = {(e) => {setEmail(e.target.value)}} />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="text" onChange= {(e) => {setPassword(e.target.value)}} />
+                </div>
+                <div>
+                    <label>Confirm Password</label>
+                    <input type="text" onChange= {(e) => {setConfirmPassword(e.target.value)}} />
+                </div>
+            </form>
+            <p>
+                First Name: {firstName} <br />
+                Last Name:{lastName} <br />
+                Email: {email} <br />
+                Password: {password} <br />
+                Confirm Password: {confirmPassword}
+            </p>
+        </div>
+    )
+}
+
+export default Form
